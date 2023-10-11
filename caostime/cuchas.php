9ceb14db7fd3ddf88666d063d101dcc8e42e427a
EmilyBaigorria <132698357+EmilyBaigorria@users.noreply.github.com>
2023-10-11 17:43:44 -0300

    Add files via upload

---
+++ caostime/cuchas.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Productos a la Venta</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>-CUCHAS PARA PERROS</h1>
+    </header>
+    <main>
+       
+        <!-- Producto 2 -->
+        <div class="producto">
+            <div class="producto-contenedor">
+                <img src="img/cama.jpg" alt="Producto 2">
+                <div class="descripcion-precio">
+                    <h2>Producto 2</h2>
+                    <p>Descripción del Producto 2.</p>
+                    <p>Precio: $1000</p>
+                    <button>Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+        
+         
+        <!-- Producto 2 -->
+        <div class="producto">
+            <div class="producto-contenedor">
+                <img src="img/img.jpg" alt="Producto 2">
+                <div class="descripcion-precio">
+                    <h2>Producto 2</h2>
+                    <p>Descripción del Producto 2.</p>
+                    <p>Precio: $1000</p>
+                    <button>Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+        <!-- Producto 4 -->
+        <div class="producto">
+            <div class="producto-contenedor">
+                <img src="img/cucha perro.jpg" alt="Producto 4">
+                <div class="descripcion-precio">
+                    <h2>Producto 4</h2>
+                    <p>Descripción del Producto 4.</p>
+                    <p>Precio: $1000</p>
+                    <button>Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+        
+   
+        <!-- Producto 6 -->
+        <div class="producto">
+            <div class="producto-contenedor">
+                <img src="img/cucha.jpg" alt="Producto 6">
+                <div class="descripcion-precio">
+                    <h2>Producto 6</h2>
+                    <p>Descripción del Producto 6.</p>
+                    <p>Precio: $1000</p>
+                    <button>Agregar al carrito</button>
+                </div>
+            </div>
+        </div>
+        
+     
+    </main>
+    <footer>
+        <p><a href="index.html">Volver a la Página Principal</a></p>
+       
+    </footer>
+</body>
+</html>
---
+++ caostime/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tu Tienda de Productos</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a Nuestra Tienda</h1>
+    </header>
+    <main>
+        <p>¡Descubre nuestros productos!</p>
+        <a href="productos.html"><button>Ver Productos</button></a>
+    </main>
+    <footer>
+        <p>Derechos de autor © 2023 Tu Tienda de Productos</p>
+    </footer>
+</body>
+</html>
